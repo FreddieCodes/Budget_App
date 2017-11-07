8d450628bce4ef8af133d2e4349b6b5230cc7f87
Freddie Ofori <FrederickOfori@email.com>
2017-11-07 16:52:08 +0000

    EventListener added to keypress Enter and button click

--- app.js
+++ app.js
@@ -13,5 +13,30 @@ var UIController = (function(){
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-   
+
+    var ctrlAddItem = function(){
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the  UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        
+       console.log('it works');
+    }
+
+   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+   // adds the Event Listener to the global
+   document.addEventListener('keypress', function(event){
+    //    .which is for older browsers
+       if (event.keyCode === 13 || event.which === 13){
+           ctrlAddItem();
+           console.log('ENTER was pressed');
+       }
+   });
+
 })(budgetController, UIController); // this controller knows about the other 2 modules as we have called them as arguments.
